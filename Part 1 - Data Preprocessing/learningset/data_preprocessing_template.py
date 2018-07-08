8076cd654c4d54ba8d7389246ea67df335da2323
Rikimarutsui <Rikimarutsui@gmail.com>
2018-07-09 01:05:08 +0800

    Section 2 Leture 10 Import Library
    Section 2 Leture 10 Import Library

---
+++ Part 1 - Data Preprocessing/learningset/data_preprocessing_template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 00:49:00 2018
+
+@author: RikimaruTsui
+"""
+
+# Machine Learning A-Z
+
+# Import Library
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 

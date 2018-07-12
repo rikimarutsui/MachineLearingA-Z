c42c7d15e553e9767d22d83782760941f45d8b4f
Rikimarutsui <Rikimarutsui@gmail.com>
2018-07-13 00:51:21 +0800

    Section 2 Lecture 13 Missing Data
    Take care of the missing data by average strategy in Python and R

--- Part 1 - Data Preprocessing/learningset/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/learningset/data_preprocessing_template.R
@@ -2,3 +2,14 @@
 
 # Import a Dataset
 dataset = read.csv('Data.csv')
+
+# Taking care a missing data
+  # if is.na = if the value is N/A it will return true
+  # take care of the missing age
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),  
+                     dataset$Age)
+  # take care of the missing salary
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
--- Part 1 - Data Preprocessing/learningset/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/learningset/data_preprocessing_template.py
@@ -19,3 +19,12 @@ dataset = pd.read_csv('Data.csv')
 # Create the matrix
 X = dataset.iloc[:, :-1].values # take all the column except last one (-1)
 y = dataset.iloc[:, 3].values
+
+
+# Take care of missing data
+# SK Learn is the library for data preprocessing
+# The Imputer this settings is using Average Strategy ('mean')
+from sklearn.preprocessing import Imputer # import the class
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) # recoginate
+imputer = imputer.fit(X[:, 1:3]) # taking colmun 1 to 2 (3 is not been taking) 
+X[:, 1:3] = imputer.transform(X[:, 1:3]) # copy the imputered data from imputer to X same positions
